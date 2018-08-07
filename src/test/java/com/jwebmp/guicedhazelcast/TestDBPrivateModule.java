3d9fccd1e03434d2548ade756d6e07e99b3ac127
GedMarc <ged_marc@hotmail.com>
2018-08-07 15:58:49 +0200

    Enhancements

--- src/test/java/com/jwebmp/guicedhazelcast/TestDBPrivateModule.java
+++ src/test/java/com/jwebmp/guicedhazelcast/TestDBPrivateModule.java
@@ -1,8 +1,8 @@
 package com.jwebmp.guicedhazelcast;
 
+import com.jwebmp.guicedpersistence.btm.BTMConnectionBaseInfo;
 import com.jwebmp.guicedpersistence.db.ConnectionBaseInfo;
 import com.jwebmp.guicedpersistence.db.connectionbasebuilders.AbstractDatabaseProviderModule;
-import com.jwebmp.guicedpersistence.jpa.JPAConnectionBaseInfo;
 import com.oracle.jaxb21.PersistenceUnit;
 
 import javax.validation.constraints.NotNull;
@@ -16,7 +16,7 @@ public class TestDBPrivateModule
 	@Override
 	protected @NotNull ConnectionBaseInfo getConnectionBaseInfo(PersistenceUnit unit, Properties filteredProperties)
 	{
-		return new JPAConnectionBaseInfo();
+		return new BTMConnectionBaseInfo();
 	}
 
 	@Override
